ec88c20f2ca66da35c26e921733eed5389ef1bf0
decoder <sshukla90@outlook.com>
2026-02-09 15:04:41 +0000

    Fix: Add required_providers to security module

--- security/main.tf
+++ security/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.0"
+    }
+  }
+}
+
 # Read and decode YAML configuration
 locals {
   config_file = file(var.config_file_path)
